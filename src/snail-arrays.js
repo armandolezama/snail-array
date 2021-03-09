1866e750c9e2269f6dc776103c62c57c08d82a15
armando <armando.lezama.psic@gmail.com>
2021-03-09 15:21:48 -0600

    feat(js): added methos to manage columns and rows, and test cases

--- src/snail-arrays.js
+++ src/snail-arrays.js
@@ -1,31 +1,53 @@
 const getValues = (from, to, array) => {
+  let values = [];
 
-}
+  if(from[0] < to[0] || from[1] < to[1]){
+    for (let row = from[0]; row <= to[0]; row++) {
+      for (let column = from[1]; column <= to[1]; column++) {
+        values = [...values, array[row][column]];
+      };
+    };
+  } else {
+    for (let row = from[0]; row >= to[0]; row--) {
+      for (let column = from[1]; column >= to[1]; column--) {
+        values = [...values, array[row][column]];
+      };
+    };
+  }
+
+  return values;
+};
 
 const snailArrays = simpleArray => {
   let xOrigin = 0;
   let yOrigin = 0;
-  let xEnd = simpleArray.length;
-  let yEnd = simpleArray[0].length;
+  let xEnd = simpleArray.length - 1;
+  let yEnd = simpleArray[0].length - 1;
   let values = [];
 
   while(xOrigin < xEnd && yOrigin < yEnd) {
     let from = [xOrigin, yOrigin];
-    let to = [xEnd, yOrigin]
-    values = [...values, getValues( from, to, simpleArray)];
-    ++yOrigin;
-    from = [...to];
-    to = [xEnd, yEnd];
-    values = [...values, getValues( from, to, simpleArray)];
-    --xEnd;
-    from = [...to];
-    to = [xOrigin, yEnd];
-    values = [...values, getValues( from, to, simpleArray)];
-    --yEnd;
-    from = [...to];
-    to = [xOrigin, yOrigin];
-    values = [...values, getValues( from, to, simpleArray)];
+    let to = [xOrigin, yEnd]
+    values = [...values, ...getValues( from, to, simpleArray)];
     ++xOrigin;
+    from = [xOrigin, yEnd];
+    to = [xEnd, yEnd];
+    values = [...values, ...getValues( from, to, simpleArray)];
+    --yEnd;
+    from = [xEnd, yEnd];
+    to = [xEnd, yOrigin];
+    values = [...values, ...getValues( from, to, simpleArray)];
+    --xEnd;
+    if(xEnd > 0) {
+      from =  [xEnd, yOrigin];
+      to = [xOrigin, yOrigin];
+      values = [...values, ...getValues( from, to, simpleArray)];
+    }
+    ++yOrigin;
+  };
+
+  if(xOrigin <= xEnd && yOrigin <= yEnd){
+    values = [...values, ...getValues([xOrigin, yOrigin], [xEnd, yEnd], simpleArray)]
   };
 
   return values;
--- test/snail-arrays.js
+++ test/snail-arrays.js
@@ -13,17 +13,34 @@ suite('Snail arrays', () => {
     })
   })
   test('get values from column', () => {
-    assert.deepEqual(getValues([0,4], [4,4], twoDimensionalArray), ['0 - 4', '1 - 4', '2 - 4', '3 - 4', '4 - 4'])
+    assert.deepEqual(getValues([0,4], [4,4], twoDimensionalArray), [['0 - 4'], ['1 - 4'], ['2 - 4'], ['3 - 4'], ['4 - 4']])
   });
   test('get values from row', () => {
-    assert.deepEqual(getValues([3, 0], [3, 4], twoDimensionalArray), ['3 - 0', '3 - 1', '3 - 2', '3 - 3', '3 - 4',])
+    assert.deepEqual(getValues([3, 0], [3, 4], twoDimensionalArray), [['3 - 0'], ['3 - 1'], ['3 - 2'], ['3 - 3'], ['3 - 4']])
   });
   test('3x3 bidimensional array', () => {
     const arrayFromTest = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
-    ]
-    assert.deepEqual(snailArrays(arrayFromTest), [1, 2, 3, 6, 9, 8, 7, 4, 5])
+    ];
+    assert.deepEqual(snailArrays(arrayFromTest), [1, 2, 3, 6, 9, 8, 7, 4, 5]);
+  });
+  test('5x2 bidimensional array', () => {
+    const arrayFromTest = [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+      [7, 8],
+      [9, 10],
+    ];
+    assert.deepEqual(snailArrays(arrayFromTest), [1, 2, 4, 6, 8, 10, 9, 7, 5, 3]);
+  });
+  test('5x2 bidimensional array', () => {
+    const arrayFromTest = [
+      [1, 2 ,3 ,4 ,5],
+      [6, 7, 8, 9, 10]
+    ];
+    assert.deepEqual(snailArrays(arrayFromTest), [1, 2, 3, 4, 5, 10, 9, 8, 7, 6]);
   });
 });
